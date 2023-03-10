97a8d0ba7e866320a05ab7b53707cc79ad62ca7e
alexhtaylor <alexhenrytaylor@gmail.com>
2023-03-10 07:43:51 +0700

    refactoring code

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,27 +1,15 @@
 class LocationsController < ApplicationController
 
   def show
-    @latitudee = params[:latitude]
-    @longitudee = params[:longitude]
-    session[:latitudee] = @latitudee
-    session[:longitudee] = @longitudee
-    puts "LATITUDE FROM LOCATIONS = #{@latitudee}"
+    params[:latitude] = session[:latitude]
+    params[:longitude] = session[:longitude]
   end
 
   def create
-    puts "INSIDE THE CREATE FUNCTION"
     update
-
-    @latitudee = session[:latitudee]
-@longitudee = session[:longitudee]
-    puts "LATITUDE FROM LOCATIONS CREATE = #{@latitudee}"
     @location = current_user.locations.new(location_params)
-    # @latitude = params[:latitude]
-    # @longitude = params[:longitude]
-    # @latitudee = params[:latitude]
-    @location.latitude = @latitudee
-    @location.longitude = @longitudee
-    # puts "latitude from location controller= #{@latitude}"
+    @location.latitude = session[:latitude]
+    @location.longitude = session[:longitude]
     # Testing
     # @location.latitude = 20
     # @location.longitude = 99
@@ -37,15 +25,6 @@ class LocationsController < ApplicationController
       puts "Failed to pin location."
       redirect_to map_path
     end
-    # respond_to do |format|
-    #   if @location.save
-    #     flash[:success] = "Location pinned!"
-    #     format.js # Render create.js.erb
-    #   else
-    #     flash[:error] = "Failed to pin location."
-    #     format.js { render :create_error } # Render create_error.js.erb
-    #   end
-    # end
   end
 
   def update
@@ -67,37 +46,3 @@ class LocationsController < ApplicationController
     params.require(:location).permit(:latitude, :longitude, :current_location, :visible)
   end
 end
-
-# class LocationsController < ApplicationController
-#   def create
-#     update
-#     @location = current_user.locations.new(location_params)
-#     @location.datetime = DateTime.current
-
-#     if @location.save
-#       flash[:success] = "Location pinned!"
-#       # Call the `addMarker` function after location is saved
-#       respond_to do |format|
-#         format.html { redirect_to map_path }
-#         format.js
-#       end
-#     else
-#       flash[:error] = "Failed to pin location."
-#       # redirect_to map_path
-#     end
-#   end
-
-#   def update
-#     @all_locations = current_user.locations
-#     @all_locations.each do |location|
-#       location.current_location = false
-#       location.save
-#     end
-#   end
-
-#   private
-
-#   def location_params
-#     params.require(:location).permit(:latitude, :longitude, :current_location, :visible)
-#   end
-# end
